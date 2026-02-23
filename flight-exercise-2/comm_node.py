5836e364bc5ec8996c00d73841665325d14ce823
Richard Wu <richkejie@gmail.com>
2026-02-23 17:59:32 -0500

    added publish waypoint; initial implementation of launch, test, land cmds; can hover

--- flight-exercise-2/comm_node.py
+++ flight-exercise-2/comm_node.py
@@ -23,6 +23,15 @@ class CommNode(Node):
         )
         self.create_timer(5, self.publish_position) # publish vision pose at 20Hz
 
+        self.waypoint_pub = self.create_publisher(
+            PoseStamped, 
+            'mavros/setpoint_position/local', 
+            qos_profile_system_default
+        )
+        self.create_timer(1/20, self.publish_waypoint) # publish waypoint at 20Hz
+
+        # I believe flight controller compares waypoint to current position
+
         # subscribers
         self.vicon_sub = self.create_subscription( # not using if no vicon...
             PoseStamped, 
@@ -47,13 +56,30 @@ class CommNode(Node):
         # pose
         self.initial_pose = None
         self.latest_pose = None
-
+        
+        if self.latest_pose is None:
+            self.waypoint_pose = self.latest_pose
+        else:
+            self.waypoint_pose = PoseStamped()
+        self.update_waypoint_pose(0.0, 0.0, 0.0)
+        
         # state
         self.state = State()
         # mavros clients
         self.arming_client = self.create_client(CommandBool, "mavros/cmd/arming")
 
         self.get_logger().info("CommNode initiailized, has not received intial pose yet.")
+
+        # Pose to hold when hover command is called
+        self.hover_pose = None
+
+    # ----------------------------- helper functions -------------------------------------------------
+    def update_waypoint_pose(self, x, y, z):
+        self.waypoint_pose.header.stamp = self.get_clock().now().to_msg()
+        self.waypoint_pose.header.frame_id = "map"
+        self.waypoint_pose.pose.position.x = x
+        self.waypoint_pose.pose.position.y = y  
+        self.waypoint_pose.pose.position.z = z  
 
     # ---------------------------- commands -------------------------------------------------
     def cmd_arm_drone(self, arm_status):
@@ -73,22 +99,29 @@ class CommNode(Node):
             response.message = "No initial pose."
             return response
         
-        # target_z = self.initial_pose.position.z + G_HEIGHT
-
-        # self.get_logger().info(f"Launch Requested. Target altitude: {G_HEIGHT}m")
-
         # arm drone if not armed yet
         if not self.state.armed:
             print("calling cmd_arm_drone")
             self.cmd_arm_drone(True)
 
+        target_z = self.initial_pose.position.z + G_HEIGHT
+
+        self.get_logger().info(f"Launch Requested. Target altitude: {G_HEIGHT}m")
+
+        # set hover pose to target altitude above initial position
+        self.update_waypoint_pose(self.waypoint_pose.pose.position.x, self.waypoint_pose.pose.position.y, target_z)     
+
         response.success = True
         response.message = "Drone taking off."
+
         return response
 
     def callback_test(self, request, response):
         """Handle TEST command: just wait until TA done collecting data..."""
         self.get_logger().info("Test Requested. Starting test sequence.")
+
+        self.update_waypoint_pose(self.latest_pose.pose.position.x, self.latest_pose.pose.position.y, self.latest_pose.pose.position.z) # maintain current position during test
+
         response.success = True
         response.message = "Test has started. Recording data."
         return response
@@ -100,8 +133,9 @@ class CommNode(Node):
             response.message = "No initial pose."
             return response
 
-        self.publish_position(self.initial_pose.position.x, self.initial_pose.position.y, self.initial_pose.position.z-0.05)
         self.get_logger().info(f"Landing Requested. Returning to z={self.initial_pose.position.z}m")
+
+        self.update_waypoint_pose(self.initial_pose.position.x, self.initial_pose.position.y, self.initial_pose.position.z-0.05) # set waypoint to just below initial position to encourage landing
 
         response.success = True
         response.message = "Drone is landing."
@@ -153,8 +187,8 @@ class CommNode(Node):
         """Store initial pose"""
         if self.initial_pose is None:
             self.initial_pose = msg.pose
-            self.publish_position(self.initial_pose.position.x, self.initial_pose.position.y, self.initial_pose.position.z)
-            self.get_logger().info(f"Initial pose recorded and set: x={self.initial_pose.position.x}, y={self.initial_pose.position.y}, z={self.initial_pose.position.z}")
+            self.get_logger().info(f"Vicon: Initial pose recorded and set: x={self.initial_pose.position.x}, y={self.initial_pose.position.y}, z={self.initial_pose.position.z}")
+        self.latest_pose = msg.pose
 
     def publish_position(self):
         """Publishes a new desired position."""
@@ -167,6 +201,11 @@ class CommNode(Node):
             pos_msg.header.frame_id = "map"
             self.ego_pos_pub.publish(pos_msg)
             self.get_logger().info(f"Published self pose")
+
+    def publish_waypoint(self):
+        self.hover_pose.header.stamp = self.get_clock().now().to_msg()
+        self.waypoint_pub.publish(self.hover_pose)
+        self.get_logger().info(f"Published waypoint: x={self.hover_pose.pose.position.x}, y={self.hover_pose.pose.position.y}, z={self.hover_pose.pose.position.z}")
 
 
 def main(args=None):
